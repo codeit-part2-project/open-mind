1ca0807a14ec4f0cdb23786946c39e21fd2fc865
Tokyun02 <developtk02@gmail.com>
2024-12-13 11:43:38 +0900

    :sparkles: feat : answers API 구현

---
+++ src/api/answers.js
@@ -0,0 +1,90 @@
+const postAnswer = async (questionId, postBody) => {
+  const BASE_URL = `${process.env.REACT_APP_BASE_URL}questions/`;
+  try {
+    const response = await fetch(`${BASE_URL}${questionId}/answers/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postBody),
+    });
+    if (!response.ok) {
+      throw new Error(`에러코드 : ${response.status}`);
+    }
+    const answer = await response.json();
+    return answer;
+  } catch ({ message }) {
+    return message;
+  }
+};
+
+const getAnswer = async (answerId) => {
+  const BASE_URL = `${process.env.REACT_APP_BASE_URL}answers/${answerId}/`;
+  try {
+    const response = await fetch(BASE_URL);
+    if (!response.ok) {
+      throw new Error(`에러코드 : ${response.status}`);
+    }
+    const answer = await response.json();
+    return answer;
+  } catch ({ message }) {
+    return message;
+  }
+};
+
+const deleteAnswer = async (answerId) => {
+  const BASE_URL = `${process.env.REACT_APP_BASE_URL}answers/`;
+  try {
+    const response = await fetch(`${BASE_URL}${answerId}/`, {
+      method: 'DELETE',
+    });
+    if (!response.ok) {
+      throw new Error(`에러코드 : ${response.status}`);
+    }
+    return response;
+  } catch ({ message }) {
+    return message;
+  }
+};
+
+const putAnswer = async (answerId, putBody) => {
+  const BASE_URL = `${process.env.REACT_APP_BASE_URL}answers/`;
+  try {
+    const response = await fetch(`${BASE_URL}${answerId}/`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(putBody),
+    });
+    if (!response.ok) {
+      throw new Error(`에러코드 : ${response.status}`);
+    }
+    const answer = await response.json();
+    return answer;
+  } catch ({ message }) {
+    return message;
+  }
+};
+
+const patchAnswer = async (answerId, putBody) => {
+  const BASE_URL = `${process.env.REACT_APP_BASE_URL}answers/`;
+  try {
+    const response = await fetch(`${BASE_URL}${answerId}/`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(putBody),
+    });
+    if (!response.ok) {
+      throw new Error(`에러코드 : ${response.status}`);
+    }
+    const answer = await response.json();
+    return answer;
+  } catch ({ message }) {
+    return message;
+  }
+};
+
+export { postAnswer, getAnswer, deleteAnswer, putAnswer, patchAnswer };
