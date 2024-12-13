e9459bed7d57d3a34b9946f64dd4e90019807894
Tokyun02 <developtk02@gmail.com>
2024-12-13 21:20:18 +0900

    :wrench: chore : AppProvider 및 ModalPortal 컴포넌트 렌더링을 App 컴포넌트로 이관

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 import App from 'components/App';
-import { AppProvider } from 'components/Context';
-import ModalPortal from 'utils/portal';
-import Modal from 'components/Modals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-      <ModalPortal>
-        <Modal />
-      </ModalPortal>
-    </AppProvider>
+    <App />
   </React.StrictMode>,
 );
