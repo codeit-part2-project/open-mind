23723918d6fe35b434aa83705183a1531efab816
junAlexx <lep240927@gmail.com>
2024-12-15 02:15:53 +0900

    :lipstick: chore : 공유하기 버튼 svg 스타일링

--- src/components/feedHeader/index.jsx
+++ src/components/feedHeader/index.jsx
@@ -5,6 +5,10 @@ import urlCopy from 'assets/images/icons/ic_Link.svg';
 import kakaotalk from 'assets/images/icons/ic_Kakaotalk.svg';
 import facebook from 'assets/images/icons/ic_Facebook.svg';
 
+const style = {
+  filter: 'invert(100%) sepia(0%) saturate(0%) hue-rotate(192deg) brightness(107%) contrast(105%)',
+};
+
 const FeedHeader = () => (
   <div className='fixed'>
     <div className='flex flex-col items-center relative'>
@@ -19,9 +23,15 @@ const FeedHeader = () => (
         {/* 프로필 이미지입니다. 이미지 src는 서버에서 데이터 요청 후 반영할 예정입니다. */}
         <div className='font-normal text-2xl/[30px] md:text-[32px]/[40px]'>이름</div>
         <div className='flex gap-3'>
-          <img src={urlCopy} alt='url_copy' />
-          <img src={kakaotalk} alt='kakaotalk_share' />
-          <img src={facebook} alt='facebook_share' />
+          <div className='flex justify-center items-center w-10 h-10 rounded-full bg-[#542F1A]'>
+            <img className='w-[18px] h-[18px]' style={style} src={urlCopy} alt='url_copy' />
+          </div>
+          <div className='flex justify-center items-center w-10 h-10 rounded-full bg-[#FEE500]'>
+            <img className='w-[18px] h-[18px]' src={kakaotalk} alt='kakaotalk_share' />
+          </div>
+          <div className='flex justify-center items-center w-10 h-10 rounded-full bg-[#1877F2]'>
+            <img className='w-[18px] h-[18px]' style={style} src={facebook} alt='facebook_share' />
+          </div>
         </div>
       </div>
     </div>
