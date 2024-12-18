f311990002056402d028cff29c4c61b1d0bde1ba
CJewon <112820744+CJewon@users.noreply.github.com>
2024-12-18 18:51:35 +0900

    Feat/무한스크롤 구현 (#37)
    * :art: refactor : intersection observer API 사용시, fetchQuestions 함수에 보내야하는 파라미터들이 추가될것 같아 fetchQuestion함수를 내부로 refactor 하는 작업을 함
    
    * :sparkles: feat : 데이터 로딩을 확인하기 위해 데이터 불러오기 버튼을 임시 생성
    
    * :sparkles: feat : 무한 스크롤 기능 구현

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getQuestionBySubjectId } from 'api/questions';
 
@@ -6,45 +6,81 @@ import messagesIcon from 'assets/images/icons/Messages.svg';
 import qusetionBoxImg from 'assets/images/img_QusetionBox.svg';
 import CountingFavorite from 'components/Feed/favorite';
 
-const fetchQuestions = async (subjectId, setQuestions, setGetLoading, setGetError) => {
-  try {
-    setGetLoading(true);
-    setGetError('');
-    const params = { limit: 10, offset: 0 };
-    const response = await getQuestionBySubjectId(subjectId, params);
-    if (response.results) {
-      setQuestions(response.results);
-    } else {
-      throw new Error('질문 데이터를 가져오는 데 실패했습니다.');
-    }
-  } catch (err) {
-    setGetError(err.message);
-  } finally {
-    setGetLoading(false);
-  }
-};
-
 const Feed = () => {
   const { id: subjectId } = useParams();
   const [questions, setQuestions] = useState([]);
-  const [getLoading, setGetLoading] = useState(true);
-
-  const [getError, setGetError] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+  const [offset, setOffset] = useState(0);
+  const [hasMore, setHasMore] = useState(false);
+  const observerRef = useRef(null);
 
   useEffect(() => {
-    fetchQuestions(subjectId, setQuestions, setGetLoading, setGetError);
-  }, [subjectId]);
+    const fetchQuestions = async () => {
+      try {
+        setLoading(true);
+        setError('');
+        const params = { limit: 3, offset };
+        const response = await getQuestionBySubjectId(subjectId, params);
+        if (response.results) {
+          setQuestions((prev) => [...prev, ...response.results]);
+          setHasMore(response.results.length > 0);
+        } else {
+          throw new Error('질문 데이터를 가져오는 데 실패했습니다.');
+        }
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  if (getLoading) return <div className='feed-loading'>로딩 중...</div>;
-  if (getError) return <div className='feed-error'>오류: {getError}</div>;
+    fetchQuestions();
+  }, [subjectId, offset]);
+
+  const loadMoreQuestions = useCallback(
+    (entries) => {
+      const [entry] = entries;
+      if (entry.isIntersecting && hasMore && !loading) {
+        setOffset((prev) => prev + 3);
+      }
+    },
+    [hasMore, loading],
+  );
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(loadMoreQuestions, {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0,
+    });
+
+    const targetCurrent = observerRef.current;
+
+    if (targetCurrent) {
+      observer.observe(targetCurrent);
+    }
+
+    return () => {
+      if (targetCurrent) {
+        observer.unobserve(targetCurrent);
+      }
+    };
+  }, [loadMoreQuestions]);
+
+  if (loading && questions.length === 0) return <div className='feed-loading'>로딩 중...</div>;
+  if (error) return <div className='feed-error'>오류: {error}</div>;
 
   return (
-    <section className='feed__section flex flex-col gap-[48px] justify-center items-center  bg-gray-20 p-[24px] pt-[176px] md:p-[32px]'>
+    <section className='feed__section flex flex-col gap-[48px] justify-center items-center bg-gray-20 p-[24px] pt-[176px] md:p-[32px]'>
       <div className='feed__container bg-brown-10 border border-brown-20 rounded-[16px] w-full pb-[16px] max-w-[327px] desktop:max-w-[716px] md:max-w-[704px]'>
         <div className='question-count__container flex justify-center items-center py-[16px] gap-[8px] '>
           <img src={messagesIcon} alt='말풍선 이미지' />
-
-          <h1 className='text-lg font-normal text-brown-40 leading-6 md:text-xl md:leading-[25px]'>{questions.length > 0 ? `${questions.length}개의 질문이 있습니다` : '아직 질문이 없습니다.'}</h1>
+          {questions.length > 0 ? (
+            <h1 className='text-lg font-normal text-brown-40 leading-6 md:text-xl md:leading-[25px]'>{questions.length}개의 질문이 있습니다</h1>
+          ) : (
+            <h1 className='text-lg font-normal text-brown-40 leading-6 md:text-xl md:leading-[25px]'>아직 질문이 없습니다.</h1>
+          )}
         </div>
         {questions.length > 0 ? (
           <ul className='feed-questions__ul flex flex-col gap-[16px]'>
@@ -70,7 +106,7 @@ const Feed = () => {
                       </div>
                     </div>
                   )}
-                  <CountingFavorite like={question.like} dislike={question.dislike} questionId={question.id} />
+                  <CountingFavorite like={question.like} dislike={question.dislike} />
                 </div>
               </li>
             ))}
@@ -78,6 +114,7 @@ const Feed = () => {
         ) : (
           <img src={qusetionBoxImg} alt='질문박스 이미지' />
         )}
+        <div ref={observerRef} className='h-1' />
       </div>
       <button type='button' className='bg-brown-40 shadow-3pt px-[24px] py-[12px] rounded-[200px] text-gray-10 text-xl font-normal leading-[25px] self-end'>
         <span className='block md:hidden'>질문 작성</span>
