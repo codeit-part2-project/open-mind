c4e206497ef37d9a3353ec55293f8825b0d52a42
Tokyun02 <developtk02@gmail.com>
2024-12-18 13:34:28 +0900

    :sparkles: feat : Pagination 컴포넌트 연동 구현

---
+++ src/components/SortDropDown/index.jsx
@@ -0,0 +1,10 @@
+const SortDropDown = () => {
+  <div className='bg-gray-10 border-gray-40 border rounded-lg border-solid px-3 py-2 text-sm text-gray-40 font-normal'>
+    <ul>
+      <li>이름순</li>
+      <li>최신순</li>
+    </ul>
+  </div>;
+};
+
+export default SortDropDown;
