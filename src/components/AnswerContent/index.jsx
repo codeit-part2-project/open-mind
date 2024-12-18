ca079adbf2570ccd47d49de91bf20abda32b9947
LMS10 <ggb7410@naver.com>
2024-12-18 13:55:41 +0900

    :sparkles: feat : 질문 대상 프로필 이미지와 이름 추가

--- src/components/AnswerContent/index.jsx
+++ src/components/AnswerContent/index.jsx
@@ -1,16 +1,18 @@
 import PropTypes from 'prop-types';
 import { useLocation } from 'react-router-dom';
 
-const AnswerContent = ({ answer }) => {
+const AnswerContent = ({ answer, name, imageSource }) => {
   AnswerContent.propTypes = {
     answer: PropTypes.shape({
       content: PropTypes.string.isRequired,
       isRejected: PropTypes.bool,
     }),
-    subject: PropTypes.shape({
-      name: PropTypes.string,
-      imageSource: PropTypes.string,
-    }),
+    name: PropTypes.string.isRequired,
+    imageSource: PropTypes.string,
+  };
+
+  AnswerContent.defaultProps = {
+    imageSource: 'https://fastly.picsum.photos/id/772/200/200.jpg?hmac=9euSj4JHTPr7uT5QWVmeNJ8JaqAXY8XmJnYfr_DfBJc',
   };
 
   const location = useLocation();
@@ -24,20 +26,31 @@ const AnswerContent = ({ answer }) => {
 
   if (isFeedPage) {
     if (answer) {
-      return <h2>{answer.content}</h2>;
+      return (
+        <>
+          <img src={imageSource} alt={`${name}의 프로필`} />
+          <p>{name}</p>
+          <p>{answer.content}</p>
+        </>
+      );
     }
     return null;
   }
 
   if (isAnswerPage) {
-    if (answer) {
-      return <h2>{answer.content}</h2>;
-    }
     return (
-      <form>
-        <textarea />
-        <button type='submit'>답변 완료</button>
-      </form>
+      <>
+        <img src={imageSource} alt={`${name}의 프로필`} />
+        <p>{name}</p>
+        {answer ? (
+          <p>{answer.content}</p>
+        ) : (
+          <form>
+            <textarea placeholder='답변을 입력해 주세요' />
+            <button type='submit'>답변 완료</button>
+          </form>
+        )}
+      </>
     );
   }
 
