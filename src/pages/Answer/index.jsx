c2d9dc8ed407998f0acf48f8f9cf73fd25c4f011
junAlexx <lep240927@gmail.com>
2024-12-18 13:10:23 +0900

    :sparkles: feat : 삭제하기 버튼 클릭 시 삭제 완료 안내 토스트 구현

--- src/pages/Answer/index.jsx
+++ src/pages/Answer/index.jsx
@@ -3,19 +3,31 @@ import ToastUrlCopy from 'components/toastUrlCopy';
 import { useState } from 'react';
 import Delete from 'pages/Answer/delete';
 import { useNavigate } from 'react-router-dom';
+import ToastDelete from 'components/toastDelete';
 // import { deleteSubject } from 'api/subjects';
 
 const Answer = () => {
-  const [isToast, setIsToast] = useState(false);
+  const [isToastUrlCopy, setIsToastUrlCopy] = useState(false);
+  const [isToastDelete, setIsToastDelete] = useState(false);
+
   const id = localStorage.getItem('id');
 
   const navigate = useNavigate();
 
-  const handleToastLoad = () => {
-    setIsToast(true);
+  const handleToastUrlCopyLoad = () => {
+    setIsToastUrlCopy(true);
 
     setTimeout(() => {
-      setIsToast(false);
+      setIsToastUrlCopy(false);
+    }, 4950);
+  };
+
+  const handleToastDelete = () => {
+    setIsToastDelete(true);
+
+    setTimeout(() => {
+      setIsToastDelete(false);
+      navigate('/');
     }, 4950);
   };
 
@@ -23,17 +35,18 @@ const Answer = () => {
     // deleteSubject(id);
     // ↑위 함수는 서버 데이터를 삭제하는 함수라서 일부로 막아뒀습니다. 삭제 동작은 확인했습니다.
     localStorage.removeItem('id');
-    navigate('/');
+    handleToastDelete();
   };
 
   return (
     <>
-      <FeedHeader onClick={handleToastLoad} />
+      <FeedHeader onClick={handleToastUrlCopyLoad} />
       <div className='pt-[322px] md:pt-[369px] flex w-screen justify-center'>
         <Delete onClick={handleDelete} id={id} />
         {/* 민서님이 작업하신 공용 컴포넌트 자리입니다. */}
       </div>
-      {isToast && <ToastUrlCopy />}
+      {isToastUrlCopy && <ToastUrlCopy />}
+      {isToastDelete && <ToastDelete />}
     </>
   );
 };
