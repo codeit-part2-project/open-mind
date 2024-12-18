a18ff84a9f9fe10a6394211fcb79932699382803
junAlexx <lep240927@gmail.com>
2024-12-18 12:15:52 +0900

    :sparkles: feat : 삭제하기 버튼 기본 레이아웃 및 스타일 작업

--- src/pages/Answer/index.jsx
+++ src/pages/Answer/index.jsx
@@ -1,6 +1,7 @@
 import FeedHeader from 'components/feedHeader';
 import Toast from 'components/toast';
 import { useState } from 'react';
+import Delete from 'pages/Answer/delete';
 
 const Answer = () => {
   const [isToast, setIsToast] = useState(false);
@@ -16,6 +17,10 @@ const Answer = () => {
   return (
     <>
       <FeedHeader onClick={handleToastLoad} />
+      <div className='pt-[322px] md:pt-[369px] flex w-screen justify-center'>
+        <Delete />
+        {/* 민서님이 작업하신 공용 컴포넌트 자리입니다. */}
+      </div>
       {isToast && <Toast />}
     </>
   );
