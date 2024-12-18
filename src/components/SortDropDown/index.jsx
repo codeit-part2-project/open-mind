5f53381f13eb6fd08347b643890572b8fa5ec85c
Tokyun02 <developtk02@gmail.com>
2024-12-18 16:42:34 +0900

    :lipstick: chore : svg 이미지 스타일링 및 스타일 수정

--- src/components/SortDropDown/index.jsx
+++ src/components/SortDropDown/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import DropDonwUpImg from 'assets/images/icons/ic_Arrow-up.svg';
-import DropDownUnderImg from 'assets/images/icons/ic_Arrow-down.svg';
+import { ReactComponent as DropDownUnderImg } from 'assets/images/icons/ic_Arrow-down.svg';
 
 const SortDropDown = ({ changeSort }) => {
   SortDropDown.propTypes = {
@@ -25,8 +25,8 @@ const SortDropDown = ({ changeSort }) => {
   return (
     <div className='relative'>
       <button type='button' className={`flex items-center gap-2 border rounded-lg py-2 px-3 text-sm  ${isOpen ? 'border-gray-60' : 'border-gray-40'} `} onClick={toggleDropdown}>
-        <span className={`text-sm ${isOpen ? 'text-gray-40' : 'text-gray-60'}`}>{sortText}</span>
-        <img src={isOpen ? DropDonwUpImg : DropDownUnderImg} alt='화살표 이미지' className='w-3.5 h-3.5' />
+        <span className={`text-sm ${isOpen ? 'text-gray-60' : 'text-gray-40'}`}>{sortText}</span>
+        {isOpen ? <img src={DropDonwUpImg} alt='위쪽 화살표' className='w-3.5 h-3.5' /> : <DropDownUnderImg className='w-3.5 h-3.5 fill-gray-40' />}
       </button>
       {isOpen && (
         <ul className='flex flex-col items-center absolute top-10 left-0 right-0 mt-2 border rounded-lg border-gray-30 bg-gray-10 text-sm cursor-pointer'>
