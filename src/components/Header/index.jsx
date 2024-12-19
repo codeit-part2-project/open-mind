fd890f2a8122756ea0df04b3f4baa8e6393a2358
junAlexx <lep240927@gmail.com>
2024-12-19 15:24:34 +0900

    :art: feat : 답변 페이지와 헤더 페이지 로직 개선 및 컴포넌트 폴더 이름 수정

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -36,7 +36,7 @@ const Header = ({ imageSource, name }) => {
 
   // ↓ URL 복사 알림 컴포넌트
   const ToastUrlCopy = () => (
-    <div className='fixed bottom-[100px] md:bottom-[60px] left-1/2 w-[167px] h-[42px] px-5 py-3 bg-black rounded-lg text-sm/[18px] font-medium text-white -translate-x-1/2 shadow-2pt animate-slide-up-fade'>
+    <div className='fixed bottom-[100px] md:bottom-[60px] left-1/2 w-[167px] h-[42px] px-5 py-3 bg-black rounded-lg text-sm/[18px] font-medium text-white -translate-x-1/2 shadow-2pt animate-slide-up-fade-urlCopy'>
       URL이 복사되었습니다
     </div>
   );
--- src/components/ToastDeleteTxt/index.jsx
+++
@@ -1,6 +0,0 @@
-const ToastDelete = () => (
-  <div className='fixed bottom-[100px] md:bottom-[60px] left-1/2 px-5 py-3 bg-black rounded-lg text-sm/[18px] font-medium text-white -translate-x-1/2 shadow-2pt animate-slide-up-fade'>
-    삭제가 완료되었습니다. 홈으로 이동합니다.
-  </div>
-);
-export default ToastDelete;
