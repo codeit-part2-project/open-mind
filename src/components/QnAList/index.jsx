cda3235a1e412d4e878667b8644ae1b768ad35e7
kimsoyoung <sy_busay@naver.com>
2024-12-20 22:04:18 +0900

    ✨  feat : 공용컴퍼넌트 적용 및 무한스크롤 기능 추가

--- src/components/QnAList/index.jsx
+++ src/components/QnAList/index.jsx
@@ -1,69 +1,45 @@
 import PropTypes from 'prop-types';
-import { useState, useEffect } from 'react';
-import { getQuestionBySubjectId } from 'api/questions';
 import AnswerStatus from 'components/AnswerStatus';
 import QuestionContent from 'components/QuestionContent';
 import AnswerContent from 'components/AnswerContent';
 import CountFavorite from 'components/CountFavorite';
 import questionBoxImg from 'assets/images/img_QuestionBox.svg';
 
-const QnAList = ({ subjectId, name, imageSource }) => {
+const QnAList = ({ name, imageSource, questionList }) => {
   QnAList.propTypes = {
-    subjectId: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     imageSource: PropTypes.string.isRequired,
+    questionList: PropTypes.arrayOf(
+      PropTypes.shape({
+        createdAt: PropTypes.string.isRequired,
+        like: PropTypes.number.isRequired,
+        dislike: PropTypes.number.isRequired,
+        answer: PropTypes.objectOf(
+          PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            questionsId: PropTypes.number.isRequired,
+            content: PropTypes.string.isRequired,
+            isRejected: PropTypes.bool.isRequired,
+            createdAt: PropTypes.string.isRequired,
+          }),
+        ).isRequired,
+      }),
+    ).isRequired,
   };
 
-  const [questionList, setQuestionList] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchQuestions = async () => {
-      try {
-        setIsLoading(true);
-        setError(null);
-        const response = await getQuestionBySubjectId(subjectId);
-        if (response.results) {
-          setQuestionList(response.results);
-        } else {
-          throw new Error('질문 목록을 불러오는 데 실패했습니다.');
-        }
-      } catch (err) {
-        setError(err.message || '알 수 없는 오류가 발생했습니다.');
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchQuestions();
-  }, [subjectId]);
-
-  if (isLoading) {
-    return <div>로딩 중...</div>;
-  }
-
-  if (error) {
-    return <div>에러: {error}</div>;
-  }
-
-  return (
-    <div>
-      {questionList.length > 0 ? (
-        <ul className='flex flex-col gap-[16px]'>
-          {questionList.map((question) => (
-            <li key={question.id} className='mx-[16px] flex flex-col gap-[24px] justify-center rounded-2xl bg-gray-10 p-[24px] shadow-1pt md:gap-[32px]'>
-              <AnswerStatus answer={question.answer} />
-              <QuestionContent createdAt={question.createdAt} content={question.content} />
-              <AnswerContent answer={question.answer} name={name} imageSource={imageSource} />
-              <CountFavorite like={question.like} dislike={question.dislike} />
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <img src={questionBoxImg} alt='질문 박스 이미지' className='mx-auto mt-[50px] mb-[86px] h-[118px] w-[114px] md:mt-[54px] md:mb-[49px] md:h-[154px] md:w-[150px]' />
-      )}
-    </div>
+  return questionList.length > 0 ? (
+    <ul className='flex flex-col gap-[16px]'>
+      {questionList.map((question) => (
+        <li key={question.id} className='mx-[16px] flex flex-col gap-[24px] justify-center rounded-2xl bg-gray-10 p-[24px] shadow-1pt md:gap-[32px]'>
+          <AnswerStatus answer={question.answer} />
+          <QuestionContent createdAt={question.createdAt} content={question.content} />
+          <AnswerContent answer={question.answer} name={name} imageSource={imageSource} />
+          <CountFavorite like={question.like} dislike={question.dislike} />
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <img src={questionBoxImg} alt='질문 박스 이미지' className='mx-auto mt-[50px] mb-[86px] h-[118px] w-[114px] md:mt-[54px] md:mb-[49px] md:h-[154px] md:w-[150px]' />
   );
 };
 
--- src/pages/Answer/index.jsx
+++ src/pages/Answer/index.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { getSubjectById, deleteSubject } from 'api/subjects';
+import { getQuestionBySubjectId } from 'api/questions';
 import Header from 'components/Header';
 import DeleteIdBtn from 'components/DeleteIdBtn';
 import CountQuestion from 'components/CountQuestion';
@@ -9,12 +10,23 @@ import ToastDeleteId from 'components/ToastDeleteId';
 import questionBoxImg from 'assets/images/img_QuestionBox.svg';
 
 const Answer = () => {
-  const { id } = useParams();
-  const [subject, setSubject] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const { id: subjectId } = useParams();
+  const [profileLoading, setProfileLoading] = useState(true);
+  const [profileError, setProfileError] = useState('');
+  const [profile, setProfile] = useState(null);
   const [isDelete, setIsDelete] = useState(false);
+  const [error, setError] = useState(null);
   const isLocalId = localStorage.getItem('id');
+
+  const [questionList, setQuestionList] = useState([]);
+
+  const [listLoading, setListLoading] = useState(false);
+
+  const [offset, setOffset] = useState(0);
+  const [hasMore, setHasMore] = useState(false);
+
+  const observerRef = useRef(null);
+
   const navigate = useNavigate();
 
   const handleToastDelete = () => {
@@ -33,7 +45,7 @@ const Answer = () => {
     //       window.confirm이 린터 규칙 문제가 있을 것 같아서 일단은 주석처리 하였습니다.
 
     try {
-      const response = await deleteSubject(id);
+      const response = await deleteSubject(subjectId);
       if (!response.ok) {
         throw new Error('삭제 중 오류가 발생했습니다. 페이지를 새로고침 합니다.');
       }
@@ -48,56 +60,103 @@ const Answer = () => {
   };
 
   useEffect(() => {
-    const fetchSubject = async () => {
+    const getProfile = async () => {
       try {
-        setIsLoading(true);
-        setError(null);
+        setProfileLoading(true);
+        setProfileError('');
         if (!isDelete) {
-          if (isLocalId) {
-            const response = await getSubjectById(id);
-            setSubject(response);
+          if (isLocalId === subjectId) {
+            setProfile(await getSubjectById(subjectId));
           } else {
             setTimeout(() => {
               navigate('/');
             }, 3000);
           }
         }
-      } catch (err) {
-        setError('질문 대상을 불러오는 중 오류가 발생했습니다.');
+      } catch (e) {
+        setProfileError(e.toString());
       } finally {
-        setIsLoading(false);
+        setProfileLoading(false);
       }
     };
 
-    fetchSubject();
-  }, [id, isDelete, isLocalId, navigate]);
+    getProfile();
+  }, [subjectId, isDelete, isLocalId, navigate]);
 
-  if (isLoading) {
-    return <div>로딩 중...</div>;
-  }
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        setListLoading(true);
 
-  if (error) {
-    return <div>에러: {error}</div>;
-  }
+        const params = { limit: 3, offset };
+        const response = await getQuestionBySubjectId(subjectId, params);
+        if (response.results) {
+          setQuestionList((prev) => [...prev, ...response.results]);
+          setHasMore(response.next !== null);
+        } else {
+          throw new Error('질문 목록을 불러오는 데 실패했습니다.');
+        }
+      } catch (err) {
+        // eslint-disable-next-line
+        console.error(err.toString());
+      } finally {
+        setListLoading(false);
+      }
+    };
 
-  if (!subject) {
-    return <div>질문 대상을 불러오는 데 실패했습니다.</div>;
-  }
+    fetchQuestions();
+  }, [subjectId, offset]);
+  const loadMoreQuestions = useCallback(
+    (entries) => {
+      const [entry] = entries;
+      if (entry.isIntersecting && hasMore && listLoading === false) {
+        setOffset((prev) => prev + 3);
+      }
+    },
+    [hasMore, listLoading],
+  );
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(loadMoreQuestions, {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0,
+    });
+    const targetCurrent = observerRef.current;
+    if (targetCurrent) {
+      observer.observe(targetCurrent);
+    }
+    return () => {
+      if (targetCurrent) {
+        observer.unobserve(targetCurrent);
+      }
+    };
+  }, [loadMoreQuestions]);
+
+  if (profileLoading) return <div className='feed-loading'>로딩 중...</div>;
+  if (profileError) return <div className='feed-error'>오류: {profileError}</div>;
+  if (error) return <div>오류: {error}</div>;
 
   return (
     <>
-      <Header imageSource={subject.imageSource} name={subject.name} />
-      <div className='flex flex-col items-center justify-center gap-[8px] md:gap-[19px] box-border bg-gray-20 pt-[145px] md:pt-[135px] p-[24px] pb-[168px] md:p-[32px] md:pb-[140px]'>
-        <DeleteIdBtn onClick={handleDelete} id={id} />
+      <Header imageSource={profile.imageSource} name={profile.name} />
+      <div className='flex flex-col items-center justify-center gap-[8px] h-full md:gap-[19px] box-border bg-gray-20 pt-[145px] md:pt-[135px] p-[24px] pb-[168px] md:p-[32px] md:pb-[140px]'>
+        <DeleteIdBtn onClick={handleDelete} id={subjectId} />
         {isDelete ? (
-          <>
+          <div className='flex flex-col justify-center w-full max-w-full bg-brown-10 border border-brown-20 rounded-[16px] pb-[16px] desktop:max-w-[716px] md:max-w-[704px]'>
             <CountQuestion count={0} />
-            <img src={questionBoxImg} alt='질문 박스 이미지' />
-          </>
+            <img src={questionBoxImg} alt='질문 박스 이미지' className='mx-auto mt-[50px] mb-[86px] h-[118px] w-[114px] md:mt-[54px] md:mb-[49px] md:h-[154px] md:w-[150px]' />
+          </div>
         ) : (
           <ul className='w-full max-w-full bg-brown-10 border border-brown-20 rounded-[16px] pb-[16px] desktop:max-w-[716px] md:max-w-[704px]'>
-            <CountQuestion count={subject.questionCount} />
-            <QnAList subjectId={subject.id} name={subject.name} imageSource={subject.imageSource} />
+            <CountQuestion count={profile.questionCount} />
+            <QnAList name={profile.name} imageSource={profile.imageSource} questionList={questionList} />
+            {listLoading && (
+              <div className='flex justify-center items-center my-10'>
+                <div className='w-10 h-10 border-4 border-t-transparent border-brown-30 rounded-full animate-spin' />
+              </div>
+            )}
+            <div ref={observerRef} className='h-1' />
           </ul>
         )}
       </div>
