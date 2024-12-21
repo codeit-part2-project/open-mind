ae859097c244f2bea14bfeadc1985909ec65695c
junAlexx <junrisexci@naver.com>
2024-12-21 13:39:46 +0900

    Feat/케밥 구현 (#51)
    * :sparkles: feat : 피드 헤더 레이아웃 구현
    
    * :lipstick: chore : 공유하기 버튼 svg 스타일링
    
    * :white_check_mark: chore : 프로필 사진 및 이름 불러오는 handleLoad 함수 구현(테스트용)
    
    * :bento: chore : 헤더이미지파일 추가 및 코드 수정
    
    * :lipstick: chore : PR 리뷰 내용 반영하여 스타일 수정
    
    * :sparkles: feat : URL 복사 알림용 토스트 구현 시작
    
    * :lipstick: chore : 토스트 기본 레이아웃 구현
    
    * :sparkles: chore : URL버튼 클릭시 클립보드에 현재 URL 복사하는 handleCopyUrl추가 및 onClick 구현
    
    * :sparkles: chore : npm prop types 다운로드, 링크 복사 버튼 클릭 시 토스트 메시지 보여주는 동작 구현
    
    * :lipstick: chore : 토스트 애니메이션 효과 구현, tailwind.config.js 수정
    
    * :lipstick: chore : 토스트 애니메이션 개선 및 tailwind.config.js 수정
    
    * :sparkles: feat : 삭제하기 버튼 기본 레이아웃 및 스타일 작업
    
    * :sparkles: feat : 삭제하기 버튼 클릭 시 로컬id 삭제 및 루트로 이동 기능
    
    * :truck: chore : toast 컴포넌트 및 파일 이름을 toastUrlCopy로 변경
    
    * :sparkles: feat : 삭제하기 버튼 클릭 시 삭제 완료 안내 토스트 구현
    
    * :art: chore : 토스트 애니메이션 동작 시간 수정 and :bug: fixed : 프로필 사진 렌더링 시 src 오류 해결
    
    * :bug: fixed: prettier 문제로 발생한 빌드 오류 수정에 관한 커밋
    
    * :truck: chore : 첫글자 소문자로 적은 폴더 이름 대문자로 변경, 기존 toast 컴포넌트와 폴더 이름을  toastUrlCopy로 변경
    
    * :truck: chore : 로컬에서 폴더명을 수정했으나 원격에서 인식하지 못해 다시 원상복구 시키는 커밋입니다.
    
    * :lipstick: chore : Answer 페이지 레이아웃 보완 및 삭제완료 토스트 애니메이션 2초로 수정
    
    * :art: chore : Answer페이지 오류처리 로직 개선(리디랙션) 및 Proptypes에 isRequired 추가
    
    * :sparkles: feat : 케밥 메뉴 레이아웃 및 동작 구현
    
    ---------
    
    Co-authored-by: junAlexx <lep240927@gmail.com>

--- src/pages/Answer/index.jsx
+++ src/pages/Answer/index.jsx
@@ -14,31 +14,20 @@ const Answer = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [isDelete, setIsDelete] = useState(false);
-  const isLocalId = localStorage.getItem('id');
+  const localId = localStorage.getItem('id');
   const navigate = useNavigate();
 
-  const handleToastDelete = () => {
-    setIsDelete(true);
-
-    setTimeout(() => {
-      navigate('/');
-    }, 3000);
-  };
-
   const handleDelete = async () => {
-    // const userConfirmed = window.confirm('정말로 삭제하시겠습니까?', ''); // user 확인작업은 confirm으로 임시로 만들었습니다.
-    // if (userConfirmed) {
-    // }
-    // NOTE: 사용자에게 정말로 삭제할 것인지 2차로 확인하는 동작 구현을 위해 작성한 코드 입니다.
-    //       window.confirm이 린터 규칙 문제가 있을 것 같아서 일단은 주석처리 하였습니다.
-
     try {
       const response = await deleteSubject(id);
       if (!response.ok) {
-        throw new Error('삭제 중 오류가 발생했습니다. 페이지를 새로고침 합니다.');
+        throw new Error('삭제 중 오류가 발생했습니다. 3초 후 페이지를 새로고침 합니다.');
       }
       localStorage.removeItem('id');
-      handleToastDelete();
+      setIsDelete(true);
+      setTimeout(() => {
+        navigate('/');
+      }, 2000);
     } catch (err) {
       setError(err.message);
       setTimeout(() => {
@@ -50,27 +39,31 @@ const Answer = () => {
   useEffect(() => {
     const fetchSubject = async () => {
       try {
-        setIsLoading(true);
-        setError(null);
         if (!isDelete) {
-          if (isLocalId) {
+          if (localId === id) {
             const response = await getSubjectById(id);
-            setSubject(response);
+            setIsLoading(true);
+            if (typeof response === 'string' && response.includes('에러')) {
+              throw new Error('존재하지 않는 피드로 접근하여 오류가 발생했습니다. 잠시 후 홈으로 이동합니다.');
+            } else {
+              setSubject(response);
+            }
           } else {
-            setTimeout(() => {
-              navigate('/');
-            }, 3000);
+            throw new Error('잘못된 접근입니다. 잠시 후 홈으로 이동합니다.');
           }
         }
       } catch (err) {
-        setError('질문 대상을 불러오는 중 오류가 발생했습니다.');
+        setError(err.message);
+        setTimeout(() => {
+          navigate('/');
+        }, 3000);
       } finally {
         setIsLoading(false);
       }
     };
 
     fetchSubject();
-  }, [id, isDelete, isLocalId, navigate]);
+  }, [id, isDelete, localId, navigate]);
 
   if (isLoading) {
     return <div>로딩 중...</div>;
@@ -80,20 +73,16 @@ const Answer = () => {
     return <div>에러: {error}</div>;
   }
 
-  if (!subject) {
-    return <div>질문 대상을 불러오는 데 실패했습니다.</div>;
-  }
-
   return (
-    <>
+    <div className='h-screen bg-gray-20'>
       <Header imageSource={subject.imageSource} name={subject.name} />
-      <div className='flex flex-col items-center justify-center gap-[8px] md:gap-[19px] box-border bg-gray-20 pt-[145px] md:pt-[135px] p-[24px] pb-[168px] md:p-[32px] md:pb-[140px]'>
+      <div className='flex flex-col items-center gap-[8px] md:gap-[19px] box-border bg-gray-20 pt-[145px] md:pt-[135px] px-[24px] md:px-[32px] '>
         <DeleteIdBtn onClick={handleDelete} id={id} />
         {isDelete ? (
-          <>
+          <div className='w-full max-w-full bg-brown-10 border border-brown-20 rounded-[16px] pb-[16px] desktop:max-w-[716px] md:max-w-[704px]'>
             <CountQuestion count={0} />
-            <img src={questionBoxImg} alt='질문 박스 이미지' />
-          </>
+            <img src={questionBoxImg} alt='질문 박스 이미지' className='mx-auto mt-[50px] mb-[86px] h-[118px] w-[114px] md:mt-[54px] md:mb-[49px] md:h-[154px] md:w-[150px]' />
+          </div>
         ) : (
           <ul className='w-full max-w-full bg-brown-10 border border-brown-20 rounded-[16px] pb-[16px] desktop:max-w-[716px] md:max-w-[704px]'>
             <CountQuestion count={subject.questionCount} />
@@ -102,7 +91,7 @@ const Answer = () => {
         )}
       </div>
       {isDelete && <ToastDeleteId />}
-    </>
+    </div>
   );
 };
 
