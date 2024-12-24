492f254c8552fb79abd269ea9f033a11e2f04edf
kimsoyoung <sy_busay@naver.com>
2024-12-24 22:07:15 +0900

    💄  style : '답변하러가기'버튼 스타일 조정 및 화살표 이미지 변경

--- src/pages/QuestionList/index.jsx
+++ src/pages/QuestionList/index.jsx
@@ -6,6 +6,7 @@ import SortDropDown from 'components/SortDropDown';
 import { getSubject } from 'api/subjects';
 import Logo from 'assets/images/img_Logo.svg';
 import useViewport from 'hooks/useViewport';
+import { ReactComponent as QnAPageMove } from 'assets/images/icons/ic_Arrow-dash-right.svg';
 
 const QuestionList = () => {
   const navigate = useNavigate();
@@ -54,12 +55,12 @@ const QuestionList = () => {
           </Link>
 
           <button
-            className='flex flex-row justify-between gap-[4px] bg-brown-10 border-brown-40 border rounded-lg px-3 py-2 text-sm text-brown-40 font-normal whitespace-nowrap transition-colors duration-300 hover:bg-brown-20'
+            className='flex flex-row items-center justify-center w-[127px] h-[34px] md:w-[166px] md:h-[46px] gap-[4px] md:gap-[8px] bg-brown-10 border-brown-40 border rounded-lg px-3 py-2 text-sm text-brown-40 font-normal md:text-base whitespace-nowrap transition-colors duration-300 hover:bg-brown-20'
             type='button'
             onClick={onClickPageMove}
           >
             <div>답변하러 가기</div>
-            <div>→</div>
+            <QnAPageMove alt='답변하러가기_화살표' />
           </button>
         </div>
       </header>
