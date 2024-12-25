3bfa7101e1c226d748e12fe2eab62cc398a9e46a
Tokyun02 <developtk02@gmail.com>
2024-12-25 22:29:57 +0900

    :lipstick: style : 삭제하기 버튼 QnAList 끝단 맞춤

--- src/components/DeleteIdBtn/index.jsx
+++ src/components/DeleteIdBtn/index.jsx
@@ -11,7 +11,7 @@ const DeleteIdBtn = ({ onClick, id }) => {
   };
 
   return (
-    <div className='relative w-full max-w-[702px] min-w-[144px] h-[25px] md:h-[35px]'>
+    <div className='relative w-full max-w-[717px] min-w-[144px] h-[25px] md:h-[35px] md:w-[702px] pc:w-[714px]'>
       <button
         type='button'
         className='absolute right-0 w-[70px] min-w-[70px] h-[25px] md:w-[100px] md:h-[35px] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] px-[17.5px] md:px-[24px] md:py-[5px] rounded-[200px] bg-brown-40 font-normal text-gray-10 text-[10px]/[25px] md:text-[15px]/[25px] hover:scale-105'
